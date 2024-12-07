a4a6b71cf1776636da92fc1a2ca9401245d2470d
marmont <mar.mont@example.com>
2024-12-07 15:47:51 +0100

    Implementierung eines Schwellenwerts und Ausblenden des Live-Action-Buttons bei Erreichen des Seitenendes
    - Schwellenwert hinzugefügt, um den Button nur bei ausreichender Scrollbarkeit anzuzeigen
    - Logik implementiert, um den Button auszublenden, wenn die Scroll-Position den unteren Rand erreicht
    - Echtzeitprüfung der Scroll-Position durch Scroll-Events und Timeout nach Smooth-Scrolling
    - Verbesserte Benutzererfahrung durch dynamisches Ein- und Ausblenden des Buttons

--- app.js
+++ app.js
@@ -1,39 +1,56 @@
 var app = angular.module('liveActionApp', []);
 
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.sampleText = new Array(9).fill('$');
+}]);
+
 app.directive('liveActionButton', ['$window', '$timeout', function ($window, $timeout) {
     return {
         restrict: 'A',
-        scope: {},
+        scope: {
+            minScrollDifference: '@'
+        },
         link: function (scope, element, attrs) {
             const button = angular.element('<button class="live-action-button"><span class="arrow-icon">&#x2193;</span></button>');
             button.css('display', 'none');
             element.append(button);
 
+            let minScrollDifference = parseInt(scope.minScrollDifference) || 50;
+
             function checkIfScrollable() {
                 $timeout(function () {
-                    var isScrollable = element[0].scrollHeight > element[0].clientHeight;
+                    let scrollableHeight = element[0].scrollHeight - element[0].clientHeight;
+                    let isScrollable = scrollableHeight > minScrollDifference;
                     button.css('display', isScrollable ? 'block' : 'none');
                 });
             }
 
+            function checkIfScrolledToBottom() {
+
+                let currentScrollPosition = element[0].scrollTop + element[0].clientHeight;
+                let isAtBottom = currentScrollPosition >= element[0].scrollHeight - 1;
+                button.css('display', isAtBottom ? 'none' : 'block');
+            }
+
             function scrollToBottom() {
                 element[0].scrollTo({ top: element[0].scrollHeight, behavior: 'smooth' });
+
+                $timeout(checkIfScrolledToBottom, 500);
             }
 
             button.on('click', scrollToBottom);
 
             angular.element($window).on('resize', checkIfScrollable);
 
+            element.on('scroll', checkIfScrolledToBottom);
+
             checkIfScrollable();
 
             scope.$on('$destroy', function () {
                 button.off('click', scrollToBottom);
                 angular.element($window).off('resize', checkIfScrollable);
+                element.off('scroll', checkIfScrolledToBottom);
             });
         }
     };
-}]);
-
-app.controller('MainController', ['$scope', function ($scope) {
-    $scope.sampleText = new Array(100).fill('This is some sample content to test scrolling.');
 }]);
